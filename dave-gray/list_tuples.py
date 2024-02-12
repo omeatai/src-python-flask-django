f08b7725b409e7bb090a944e79f27d9397fb4a39
omeatai <omeatai@gmail.com>
2024-02-11 20:50:11 -0700

    7. Python Tuples
    7. Python Tuples

--- dave-gray/list_tuples.py
+++ dave-gray/list_tuples.py
@@ -1,118 +1,27 @@
-users = ['Dave', 'John', 'Sara']
-data = ['Dave', 42, True]
-empty_list = []
+# Tuples
+my_tuple = tuple(('Dave', 42, True))
+print(my_tuple)
 
-print("Dave" in users)
-print("Dave" in data)
-print("Dave" in empty_list)
+another_tuple = (1, 4, 2, 8, 2, 2)
+print(another_tuple)
+
+print(type(my_tuple))
+print(type(another_tuple))
 print("")
 
-print(users[0])
-print(users[-2])
+new_list = list(my_tuple)
+print(new_list)
+new_list.append('Neil')
+print(new_list)
+new_tuple = tuple(new_list)
+print(new_tuple)
 print("")
 
-# FInd index of value
-print(users.index('Sara'))
-print("")
-
-# Slice from List
-print(users[0:2])
-print(users[1:])
-print(users[-3:-1])
-print(users[:])
-print("")
-
-# Find length of List
-print(len(data))
-print("")
-
-# Append/Extend to List
-users.append('Elsa')
-print(users)
-
-users += ['Jason']
-print(users)
-
-users.extend(['Robert', 'Jimmy'])
-print(users)
-
-data = ['Alex', 'Peter']
-users.extend(data)
-print(users)
-print("")
-
-# Insert to List
-users = ['Fred', 'John']
-
-users.insert(0, 'Bob')
-print(users)
-
-users[2:2] = ['Eddie', 'Alex', 'Bob', 'Stewart']
-print(users)
-
-users[2:4] = ['Robert', 'JPJ']
-print(users)
-print("")
-
-# Remove from List
-users.remove('Bob')
-print(users)
-
-popped = users.pop()
-print(users)
-print(popped)
-
-popped = users.pop(2)
-print(users)
-print(popped)
-
-del users[0]
-print(users)
-
-data.clear()  # del data
-print(data)
-print("")
-
-# Sort List
-users = ['Bob', 'Fred', 'John']
-print(users)
-
-users[1:2] = ['eddie']
-print(users)
-
-users.sort()
-print(users)
-
-users.sort(key=str.lower)
-print(users)
-print("")
-
-nums = [4, 42, 78, 1, 5]
-print(nums)
-nums.reverse()
-print(nums)
-
-print(sorted(nums, reverse=True))
-print(nums)
-
-# nums.sort(reverse=True)
-# print(nums)
-
-# Copy List
-nums = [4, 42, 78, 1, 5]
-
-nums_copy1 = nums.copy()
-print(nums_copy1)
-
-nums_copy2 = list(nums)
-print(nums_copy2)
-
-nums_copy3 = nums[:]
-print(nums_copy3)
-
-nums.sort()
-print(nums)
-print(type(nums))
-
-my_list = list([1, "Neil", True])
-print(my_list)
+# unpacking Tuples
+another_tuple = (1, 4, 2, 8, 2, 2)
+print(another_tuple)
+(one, *two, hey) = another_tuple
+print(one)
+print(two)
+print(hey)
+print(another_tuple.count(2))
