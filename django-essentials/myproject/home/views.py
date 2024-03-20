6f788cf2b56c91f44b960178d8576c35bbebd11a
omeatai <omeatai@gmail.com>
2024-03-20 00:06:49 -0600

    4. Creating your first Django template
    4. Creating your first Django template

--- django-essentials/myproject/home/views.py
+++ django-essentials/myproject/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    # return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, 'home/welcome.html', {'name': 'John Doe', 'date': datetime.now()})
