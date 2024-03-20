09253493e7a3e2e425681639f18e15110a5d6092
omeatai <omeatai@gmail.com>
2024-03-20 17:09:14 -0600

    9. Create Notes Model
    9. Create Notes Model

--- django-essentials/myproject/notes/models.py
+++ django-essentials/myproject/notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
