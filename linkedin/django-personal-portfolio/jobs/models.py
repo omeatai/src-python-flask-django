780144938397964962e4b786cfd724dc145045a8
omeatai <omeatai@gmail.com>
2024-03-27 20:26:59 -0600

    4. Creating Models in Django
    4. Creating Models in Django

--- linkedin/django-personal-portfolio/jobs/models.py
+++ linkedin/django-personal-portfolio/jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # date = models.DateField()
+    # description = models.TextField()
+    # url = models.URLField()
+    # company = models.CharField(max_length=100)
+    # position = models.CharField(max_length=100)
+    # location = models.CharField(max_length=100)
