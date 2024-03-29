c74f54e88a57b4951d882e75039720ef802a5794
omeatai <omeatai@gmail.com>
2024-03-28 23:36:14 -0600

    14. Setup URL, View and Template for Details Page
    14. Setup URL, View and Template for Details Page

--- linkedin/django-personal-portfolio/jobs/views.py
+++ linkedin/django-personal-portfolio/jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 # Create your views here.
 
@@ -6,3 +6,9 @@ from .models import Job
 def home(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+
+def job_detail(request, job_id):
+    # job = Job.objects.get(id=job_id)
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/job_detail.html', {'job': job})
