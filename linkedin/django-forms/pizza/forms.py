db771cdf8a0227c81712ed81236793b2866aff0d
omeatai <omeatai@gmail.com>
2024-03-29 13:07:32 -0600

    9. Using Model Forms
    9. Using Model Forms

--- linkedin/django-forms/pizza/forms.py
+++ linkedin/django-forms/pizza/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from .models import Pizza
+
+# CHOICES = [('small', 'Small'), ('medium', 'Medium'), ('large', 'Large')]
 
 
-CHOICES = [('small', 'Small'), ('medium', 'Medium'), ('large', 'Large')]
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     size = forms.ChoiceField(label='Size', choices=CHOICES)
 
-
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length=100)
-    topping2 = forms.CharField(label='Topping 2', max_length=100)
-    size = forms.ChoiceField(label='Size', choices=CHOICES)
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {
+            'topping1': 'Topping 1',
+            'topping2': 'Topping 2',
+            'size': 'Size',
+        }
