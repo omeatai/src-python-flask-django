c5141aba40c0f8e13be4e8cf1bc081aeec1810e4
omeatai <omeatai@gmail.com>
2024-03-29 08:49:59 -0600

    3. Create Homepage and Order Page
    3. Create Homepage and Order Page

--- linkedin/django-forms/pizza/views.py
+++ linkedin/django-forms/pizza/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'pizza/home.html')
+
+
+def order(request):
+    return render(request, 'pizza/order.html')
