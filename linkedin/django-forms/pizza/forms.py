9d3d91c02d14ecab171e51fb4410f5f81cefa426
omeatai <omeatai@gmail.com>
2024-03-29 17:10:04 -0600

    11. Using Widgets for Model Form
    11. Using Widgets for Model Form

--- linkedin/django-forms/pizza/forms.py
+++ linkedin/django-forms/pizza/forms.py
@@ -1,33 +1,39 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 CHOICES = [('small', 'Small'), ('medium', 'Medium'), ('large', 'Large')]
 TOPPING_CHOICES = [('pep', 'Pepperoni'), ('cheese',
                                           'Cheese'), ('olives', 'Olives')]
 
 
-class PizzaForm(forms.Form):
-    topping_1 = forms.CharField(label='Topping_1', max_length=100, widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    topping_2 = forms.CharField(label='Topping_2', max_length=100, widget=forms.Textarea(
-        attrs={'class': 'form-control'}))
-    topping_3 = forms.CharField(
-        label='Topping_3', max_length=100, widget=forms.PasswordInput)
-    topping_4 = forms.MultipleChoiceField(
-        label='Topping_4', choices=TOPPING_CHOICES)
-    topping_5 = forms.MultipleChoiceField(
-        label='Topping_5', choices=TOPPING_CHOICES, widget=forms.CheckboxSelectMultiple)
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     size = forms.ChoiceField(label='Size', choices=CHOICES)
 
-    topping1 = forms.CharField(label='Topping 1', max_length=100)
-    topping2 = forms.CharField(label='Topping 2', max_length=100)
-    size = forms.ChoiceField(label='Size', choices=CHOICES)
+class PizzaForm(forms.ModelForm):
 
-# class PizzaForm(forms.ModelForm):
-#     class Meta:
-#         model = Pizza
-#         fields = ['topping1', 'topping2', 'size']
-#         labels = {
-#             'topping1': 'Topping 1',
-#             'topping2': 'Topping 2',
-#             'size': 'Size',
-#         }
+    size = forms.ModelChoiceField(
+        # queryset=Size.objects.all(), to_field_name='title', empty_label=None, widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}))
+        queryset=Size.objects.all(), to_field_name='title', empty_label=None, widget=forms.RadioSelect(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {
+            'topping1': 'Topping 1',
+            'topping2': 'Topping 2',
+            'size': 'Size',
+        }
+
+        # widgets = {
+        #     'topping1': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'topping2': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'size': forms.Select(attrs={'class': 'form-control'}),
+        # }
+
+        # widgets = {
+        #     'topping1': forms.Textarea(attrs={'class': 'form-control'}),
+        #     'topping2': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'size': forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}),
+        # }
