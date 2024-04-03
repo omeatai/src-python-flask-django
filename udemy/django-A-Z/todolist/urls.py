7506fdea026753f438e4a52285ba2999620511c3
omeatai <omeatai@gmail.com>
2024-04-03 17:01:43 -0600

    6. Base Templating with Jinja 2
    6. Base Templating with Jinja 2

--- udemy/django-A-Z/todolist/urls.py
+++ udemy/django-A-Z/todolist/urls.py
@@ -3,4 +3,6 @@ from todolist import views
 
 urlpatterns = [
     path('', views.todolist, name="todolist"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
 ]
--- udemy/django-A-Z/todolist/views.py
+++ udemy/django-A-Z/todolist/views.py
@@ -4,5 +4,22 @@ from django.http import HttpResponse
 
 
 def todolist(request):
+    context = {
+        "welcome_text": "Welcome to your Todo List!"
+    }
     # return HttpResponse("<h1>Welcome to the Task Page</h1>")
-    return render(request, 'todolist.html', {})
+    return render(request, 'todolist.html', context)
+
+
+def about(request):
+    context = {
+        "welcome_text": "Welcome to the About Page!"
+    }
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {
+        "welcome_text": "Welcome to the Contact Page!"
+    }
+    return render(request, 'contact.html', context)
