d3985ea132dfa34bb55905b46db68e27b9d763b3
omeatai <omeatai@gmail.com>
2024-04-04 09:26:20 -0600

    9. Setup Django Model and Migration
    9. Setup Django Model and Migration

--- udemy/django-A-Z/todolist/models.py
+++ udemy/django-A-Z/todolist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TaskList(models.Model):
+    task = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+    # description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.task} - {self.done}"
