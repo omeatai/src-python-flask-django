071b0e57d5f1903d870b67c2bec349bef8770c9b
omeatai <omeatai@gmail.com>
2024-04-06 16:08:18 -0600

    18. Setup User Authentication App
    18. Setup User Authentication App

--- udemy/django-A-Z/taskmate/settings.py
+++ udemy/django-A-Z/taskmate/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # apps
     'todolist',
+    'user_auth',
 ]
 
 MIDDLEWARE = [
---
+++ udemy/django-A-Z/user_auth/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from user_auth import views
+
+urlpatterns = [
+    path('register/', views.register, name="register"),
+
+    # path('signup/', views.signup, name="signup"),
+    # path('login/', views.login_user, name="login"),
+    # path('logout/', views.logout_user, name="logout"),
+    # path('profile/', views.profile, name="profile"),
+    # path('edit-profile/', views.edit_profile, name="edit-profile"),
+    # path('change-password/', views.change_password, name="change-password"),
+    # path('reset-password/', views.reset_password, name="reset-password"),
+    # path('reset-password-done/', views.reset_password_done, name="reset-password-done"),
+    # path('reset-password-confirm/<uidb64>/<token>/', views.reset_password_confirm, name="reset-password-confirm"),
+    # path('reset-password-complete/', views.reset_password_complete, name="reset-password-complete"),
+    # path('delete-account/', views.delete_account, name="delete-account"),
+]
