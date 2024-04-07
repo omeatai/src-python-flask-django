d3c63eda49bdb6f0cfd92c935930d850010d4126
omeatai <omeatai@gmail.com>
2024-04-06 19:12:45 -0600

    20. Add Email Field to Form with forms.py
    20. Add Email Field to Form with forms.py

--- udemy/django-A-Z/user_auth/views.py
+++ udemy/django-A-Z/user_auth/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import CustomRegistrationForm
 # Create your views here.
 
 
 def register(request):
     if request.method == "POST":
-        register_form = UserCreationForm(request.POST)
+        register_form = CustomRegistrationForm(request.POST)
         if register_form.is_valid():
             register_form.save()
             messages.success(
@@ -18,5 +20,5 @@ def register(request):
                 request, "Sorry! Your new account could not be created. Please try again.")
             return render(request, 'user_auth/register.html', {'register_form': register_form})
     else:
-        register_form = UserCreationForm()
+        register_form = CustomRegistrationForm()
         return render(request, 'user_auth/register.html', {'register_form': register_form})
