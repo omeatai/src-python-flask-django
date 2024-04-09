9ea3288a858fe8e5a453b400794e894cfe16a213
omeatai <omeatai@gmail.com>
2024-04-08 20:06:49 -0600

    21. Redesign the Register Page with Crispy Forms
    21. Redesign the Register Page with Crispy Forms

--- udemy/django-A-Z/taskmate/settings.py
+++ udemy/django-A-Z/taskmate/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     # apps
     'todolist',
     'user_auth',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
